9dc44192c0fd396dd5cda3e68d6b41f935aedf06
Rafael Cremasco <cremasco.rafafael@gmail.com>
2025-02-04 11:44:25 -0300

    Delete src directory

--- src/main/java/com/sesi/projeto/dto/ProdutoDto.java
+++
@@ -1,6 +0,0 @@
-package com.sesi.projeto.dto;
-
-
-public record ProdutoDto(String nome, double preco, String descricao) {
-
-}
--- src/main/java/com/sesi/projeto/services/ProdutoService.java
+++
@@ -1,5 +0,0 @@
-package com.sesi.projeto.services;
-
-public class ProdutoService {
-
-}
